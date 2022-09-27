b1bfa39a5916b1dda33f1c886e277fab8da51e5f
Mohit Khotani <mohit.khotani@vimeo.com>
2022-09-27 23:07:51 +0530

    pubsub better version

--- pubsub.cpp
+++ pubsub.cpp
@@ -1,27 +1,95 @@
 #include<iostream>
-#include<unordered_map>
+#include<map>
+#include<vector>
 using namespace std;
 
-class AckHandler{
-    public:
-        virtual void onAck() = 0;
-        virtual void onNack() = 0;
-};
-
-class Message : AckHandler{
+class Message{
     private:
-        int id;
-        string publish_time;
-        int delievery_attempt;
+        string message;
+        int orderingKey;
+        string publishTime;
+        int delieveryAttempt;
+        IAckHandler ackh;
     public:
-        string data;
-        unordered_map<string, string> tags;
-        int ordering_key;     
-        void onAck(){
-            return;
+        string getMessage(){
+            return this->message;
         }
-        void onNack(){
-            return;
-        }        
 };
 
+//Interface
+class IAckHandler{
+    public:
+      virtual void OnAck() = 0;
+      virtual void OnNack() = 0; 
+};
+
+class TopicSubsriber{
+    private:
+        std::atomic<int> offset;
+        ISubsciber subscriber;
+    public:
+        TopicSubsriber(ISubsciber subscriber){
+            this->subscriber = subscriber;
+            this->offset = 0;
+        }
+};
+
+class Topic{
+    private:
+        string name;
+        string ID;
+        vector<Message> messages;
+        vector<TopicSubsriber> subscribers;
+    public:
+        Topic(){
+            
+        }
+
+        void addMessage(Message message){
+            messages.push_back(message);
+        }
+
+        void addSubscriber(TopicSubsriber subsciber){
+            subscribers.push_back(subsciber);
+        }
+};
+
+// Interface
+class ISubsciber{
+    public:
+        virtual string getID() = 0;
+        virtual void consume(Message message) = 0;
+};
+
+class TopicHandler{
+    private:
+        Topic topic;
+    public:
+        void publish(){
+        }
+};
+
+class Queue{
+  private:
+    map<string, TopicHandler> topicProcessors; // to maintain multiple topic processors
+  public:
+    Queue(){
+        
+    } 
+
+    Topic createTopic(string topicName){
+        Topic topic;
+        return topic;
+    }
+    
+    void subscribe(Topic topic, ISubsciber subscriber){
+        topic.addSubscriber(TopicSubsriber(subscriber));
+        return;
+    }
+    
+    void publish(Topic topic, Message message){
+        topic.addMessage(message);
+        return;
+    }
+
+};
