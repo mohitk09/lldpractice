12df5c6c5f41859ffb13453c4b951a4ed647c2c2
Mohit Khotani <mohit.khotani@vimeo.com>
2024-01-16 23:28:00 +0530

    implement dispense prodct

--- VendingMachine/VendingStates/Implementation/Dispense.php
+++ VendingMachine/VendingStates/Implementation/Dispense.php
@@ -2,6 +2,22 @@
 
 namespace VendingStates;
 
+use VendingMachine\Item;
+use VendingMachine\VendingMachine;
+
 class Dispense implements State
 {
+    public function __construct(VendingMachine $vendingMachine, int $code)
+    {
+        $this->dispenseProduct($vendingMachine, $code);
+    }
+
+    public function dispenseProduct(VendingMachine $machine, int $codeNumber): Item
+    {
+        print_r('dispensing product');
+        $item = $machine->inventory->getItem($codeNumber);
+        // update sold out item
+        $machine->state = new IdleState($machine);
+        return $item;
+    }
 }
