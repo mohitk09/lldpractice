51e1c570b3a014369af3e407b6eaaff3fcf5a20f
Mohit Khotani <mohit.khotani@vimeo.com>
2024-02-08 22:23:32 +0530

    balance sheet controller dummy methods

--- Splitwise/BalanceSheetController.php
+++ Splitwise/BalanceSheetController.php
@@ -2,6 +2,15 @@
 
 namespace Splitwise;
 
+use Splitwise\User\User;
+
 class BalanceSheetController
 {
+    public function updateUserExpenseBalanceSheet(): void
+    {
+    }
+
+    public function showBalanceSheetofTheUser(User $user): void
+    {
+    }
 }
